27cfa65a4d1fed3f59835a9d669aa5e3e6b5af95
dermanu <emanuellorenz@yahoo.de>
2024-11-18 14:03:50 +0100

    Update settings

--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Run `run_job_Mono.slurm` twice
-for ((i=1; i<=4; i++)); do
+for ((i=1; i<=10; i++)); do
   sbatch run_job_Mono.slurm
 done
 
 # Run `run_job_Multi.slurm` twice
-for ((j=1; j<=4; j++)); do
+for ((j=1; j<=10; j++)); do
   sbatch run_job_Multi.slurm
 done
