0157d30b3a8e9422b5db7db9760f042774cd22d1
dermanu <emanuellorenz@yahoo.de>
2024-11-20 11:11:14 +0100

    Fixed path

--- models/mediapipeMono.py
+++ models/mediapipeMono.py
@@ -5,17 +5,21 @@ import numpy as np
 import time
 from utils.frameAugmentation import FrameAugmentor
 import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
 
 import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
 
+BaseOptions = mp.tasks.BaseOptions
+PoseLandmarker = mp.tasks.vision.PoseLandmarker
+PoseLandmarkerOptions = mp.tasks.vision.PoseLandmarkerOptions
+VisionRunningMode = mp.tasks.vision.RunningMode
 
 # Load the pose landmarker model once to avoid reloading it multiple times
-options = mp.tasks.vision.PoseLandmarkerOptions(
-    base_options=mp.tasks.BaseOptions(model_asset_path='models/pose_landmarker_heavy.task'),
-    running_mode=mp.tasks.vision.RunningMode.IMAGE)
-PoseLandmarker = mp.tasks.vision.PoseLandmarker.create_from_options(options)
-
+options = PoseLandmarkerOptions(
+    base_options=BaseOptions(model_asset_path='models/pose_landmarker_heavy.task'),
+    running_mode=VisionRunningMode.IMAGE)
 
 # Currently runs on CPU as per default.
 def inference_video(cap, sweep_config=None, dimensions=3):
@@ -27,61 +31,62 @@ def inference_video(cap, sweep_config=None, dimensions=3):
     inference_time = []
     frame_number = 0
 
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            break
+    with PoseLandmarker.create_from_options(options) as landmarker:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
 
-        # Convert the BGR image to RGB - Is this really needed?
-        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            # Convert the BGR image to RGB - Is this really needed?
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        if sweep_config is not None:
-            # Augment frame
-            rgb_frame = frameaug.augment_frames(rgb_frame, sweep_config)
+            if sweep_config is not None:
+                # Augment frame
+                rgb_frame = frameaug.augment_frames(rgb_frame, sweep_config)
 
-        # Record start time
-        start_time = time.time()
+            # Record start time
+            start_time = time.time()
 
-        # Convert the frame to a MediaPipe Image object
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+            # Convert the frame to a MediaPipe Image object
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
 
-        # Detect pose landmarks from the input image
-        results = PoseLandmarker.detect(mp_image)
+            # Detect pose landmarks from the input image
+            results = landmarker.detect(mp_image)
 
-        # Record end time
-        end_time = time.time()
+            # Record end time
+            end_time = time.time()
 
-        inference_time.append(end_time - start_time)
+            inference_time.append(end_time - start_time)
 
-        # If pose detected, save keypoints to data list
-        if results.pose_world_landmarks:
-            landmarks = results.pose_world_landmarks
+            # If pose detected, save keypoints to data list
+            if results.pose_world_landmarks:
+                landmarks = results.pose_world_landmarks
 
-            for idx in range(len(landmarks)):
-                pose_landmarks = landmarks[idx]
+                for idx in range(len(landmarks)):
+                    pose_landmarks = landmarks[idx]
 
-                # Save array dependent on wanted dimensions
+                    # Save array dependent on wanted dimensions
+                    if dimensions == 3:
+                        frame_data = np.array([[i, landmark.x, landmark.y, landmark.z]
+                                               for i, landmark in enumerate(pose_landmarks)])
+                    elif dimensions == 2:
+                        frame_data = np.array([[i, landmark.x, landmark.y] for i, landmark in enumerate(pose_landmarks)])
+                    keypoints_data.append(frame_data)
+
+            else:
+                # If no pose landmarks are detected, append NaN array
                 if dimensions == 3:
-                    frame_data = np.array([[i, landmark.x, landmark.y, landmark.z]
-                                           for i, landmark in enumerate(pose_landmarks)])
+                    nan_frame_data = np.full((33, 4), np.nan)  # Assuming 33 landmarks with (index, x, y, z)
+
                 elif dimensions == 2:
-                    frame_data = np.array([[i, landmark.x, landmark.y] for i, landmark in enumerate(pose_landmarks)])
-                keypoints_data.append(frame_data)
+                    nan_frame_data = np.full((33, 3), np.nan)  # Assuming 33 landmarks with (index, x, y)
 
-        else:
-            # If no pose landmarks are detected, append NaN array
-            if dimensions == 3:
-                nan_frame_data = np.full((33, 4), np.nan)  # Assuming 33 landmarks with (index, x, y, z)
+                keypoints_data.append(nan_frame_data)
 
-            elif dimensions == 2:
-                nan_frame_data = np.full((33, 3), np.nan)  # Assuming 33 landmarks with (index, x, y)
+            frame_number += 1
 
-            keypoints_data.append(nan_frame_data)
-
-        frame_number += 1
-
-    # Release resources
-    cap.release()
+        # Release resources
+        cap.release()
 
     # Return keypoints data to NumPy array, save last frame for logging
     return np.array(keypoints_data), np.array(inference_time), rgb_frame
